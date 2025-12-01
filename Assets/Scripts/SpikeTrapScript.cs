3944e9159f21d1a297a79bcdf454396a9da508b3
McFleen <joel.dahlberg2058@gmail.com>
2025-12-01 12:14:00 +0100

    implemented spear sound

--- Assets/Scripts/SpikeTrapScript.cs
+++ Assets/Scripts/SpikeTrapScript.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 
 public class SpikeTrapScript : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.tag == "Player")
         {
             animator.SetBool("IsClose", true);
@@ -16,5 +25,10 @@ public class SpikeTrapScript : MonoBehaviour
         {
             animator.SetBool("IsClose", false);
         }
+    }
+
+    public void PlaySpearSound()
+    {
+        audio.Play();
     }
 }
