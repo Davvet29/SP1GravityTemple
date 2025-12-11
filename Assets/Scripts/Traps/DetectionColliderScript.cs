85ded96463baff97ebe424af6d677b1603fe258f
Davvet29 <Trossingdavid@gmail.com>
2025-12-11 14:25:15 +0100

    background

--- Assets/Scripts/Traps/DetectionColliderScript.cs
+++ Assets/Scripts/Traps/DetectionColliderScript.cs
@@ -4,15 +4,6 @@ using UnityEngine.Events;
 public class DetectionColliderScript : MonoBehaviour
 {
     public UnityEvent ActivateSpikeTrap;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
