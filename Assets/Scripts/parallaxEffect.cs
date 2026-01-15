97dbe6981fcea22df6bac847f06d615828f6b2cd
a25ivasa <a25ivasa@student.his.se>
2026-01-15 16:34:55 +0100

    Did some fixes that didn't work

---
+++ Assets/Scripts/parallaxEffect.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class parallaxEffect : MonoBehaviour
+{
+    private float startPos;
+    public GameObject cam;
+    public float parallaxEffectSpeed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffectSpeed;
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
